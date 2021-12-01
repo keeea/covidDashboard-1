d59db4ce6426b3e62341e499b7ca0d4cadd6025a
Anran Zheng <85720119+Anran0716@users.noreply.github.com>
2021-12-01 14:31:06 -0500

    Add files via upload

--- airflow/dags/data_pipeline/load_wk_all.py
+++ airflow/dags/data_pipeline/load_wk_all.py
@@ -41,7 +41,7 @@ def main(ds):
     )
 
     df = pd.read_csv(local_path)
-    df.to_gbq(f'covid.wk_death_age',if_exists='replace')
+    df.to_gbq(f'covid.wk_hosp_age',if_exists='replace')
 
     local_path = gcs_to_local_file(
         gcs_bucket_name='anranz_cloudservices',
