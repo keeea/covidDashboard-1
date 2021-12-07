c16756a4e14fedf871e2a23c878147955a0117ae
keeea <lexi.xiao@outlook.com>
2021-12-07 03:02:38 -0500

    update report sql

--- airflow/dags/data_pipeline/sql/final/new_report.sql
+++ airflow/dags/data_pipeline/sql/final/new_report.sql
@@ -1,6 +1,7 @@
 with recent_7day_avg as (
     select 
         PARSE_DATE("%m/%d/%Y", date_of_interest) as date_of_interest,
+        "date_is_different" as key,
         CASE_COUNT_7DAY_AVG,
         HOSP_COUNT_7DAY_AVG,
         DEATH_COUNT_7DAY_AVG
@@ -11,6 +12,7 @@ with recent_7day_avg as (
 past_14to21day_avg as (
     select 
         DATE_SUB(current_date, INTERVAL 7 DAY) as yesterday,
+        "date_is_different" as key,
         PARSE_DATE("%m/%d/%Y", date_of_interest) as p_date_of_interest,
         CASE_COUNT_7DAY_AVG as p_CASE_COUNT_7DAY_AVG,
         HOSP_COUNT_7DAY_AVG as p_HOSP_COUNT_7DAY_AVG,
@@ -21,7 +23,8 @@ past_14to21day_avg as (
 
 report_1 as (
     select 
-        a.date_of_interest, 
+        a.date_of_interest,
+        "date_is_different" as key, 
         a.CASE_COUNT_7DAY_AVG,
         a.HOSP_COUNT_7DAY_AVG,
         a.DEATH_COUNT_7DAY_AVG,
@@ -31,19 +34,32 @@ report_1 as (
 
     from recent_7day_avg as a
     full join past_14to21day_avg as b
-    on a.date_of_interest = b.yesterday 
+    on a.key = b.key
 )
 
 select 
-    a.CASE_COUNT_7DAY_AVG,
-    a.HOSP_COUNT_7DAY_AVG,
-    a.DEATH_COUNT_7DAY_AVG,
-    a.case_change,
-    a.hosp_change,
-    a.death_change,
-    b.case_count_total,
-    b.hosp_count_total,
-    b.death_count_total
+    "Case" as Type,
+    a.CASE_COUNT_7DAY_AVG as Count_7Day_Avg,
+    a.case_change as Change,
+    b.case_count_total as Count_Total,
 from report_1 a  
 full join `elated-guild-327520.staging.report_total` b
-on a.date_of_interest=b.yesterday
+on a.key=b.key
+union all
+select
+    "Hospitalization" as Type,
+    a.HOSP_COUNT_7DAY_AVG as Count_7Day_Avg,
+    a.hosp_change as Change,
+    b.hosp_count_total as Count_Total,
+from report_1 a  
+full join `elated-guild-327520.staging.report_total` b
+on a.key=b.key
+union all
+select
+    "Death" as Type,
+    a.DEATH_COUNT_7DAY_AVG as Count_7Day_Avg,
+    a.death_change as Change,
+    b.death_count_total as Count_Total
+from report_1 a  
+full join `elated-guild-327520.staging.report_total` b
+on a.key=b.key
