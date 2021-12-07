43871f9735f44ed7bd3f0aa4c623a1d1ab2dec37
keeea <lexi.xiao@outlook.com>
2021-12-07 13:57:37 -0500

    Solve table problem, add data resource  add one more map
    I have change a little in the layout grid, but still need adjustment when merge

--- airflow/dags/report_generator/generate_report1.py
+++ airflow/dags/report_generator/generate_report1.py
@@ -34,7 +34,7 @@ def main(ds):
         wk_all=wk_all_df.to_dict('list'),
         daily_summary=daily_summary_df.to_dict('list'),
         covid_map=covid_map_gdf.to_json(),
-        new_report=new_report_df.to_dict('list'),
+        new_report=new_report_df.to_dict('records'),
         top_test=top_test_df.to_dict('records'),
         vac_accumulated_by_day=vac_accumulated_by_day_df.to_dict('list'),
         breakthrough=breakthrough_df.to_dict('list'),
