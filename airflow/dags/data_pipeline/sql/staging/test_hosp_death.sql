d03561d00ec86bbe23fa6fe6c30d10db36de22db
keeea <lexi.xiao@outlook.com>
2021-12-07 04:47:08 -0500

    Change ELT
    Add one more data resource of daily case and death by geometry, joining with covid_map. Extracting, loading
     and sql files has changed for this. Three new indexed has been added in the covid_map.

--- airflow/dags/data_pipeline/sql/staging/test_hosp_death.sql
+++ airflow/dags/data_pipeline/sql/staging/test_hosp_death.sql
@@ -3,6 +3,7 @@ with test_7d as (
         modzcta_name,
         modzcta,
         percentpositivity_7day,
+        people_positive,
         median_daily_test_rate
     from `elated-guild-327520.covid.test_7d`
 ),
@@ -16,18 +17,30 @@ hosp_death_28d as (
         death_rate_28day,
         daterange
     from `elated-guild-327520.covid.hosp_death_28d`
+),
+case_death_1d as (
+    select 
+    MODIFIED_ZCTA as modzcta,
+    COVID_DEATH_COUNT,
+    COVID_DEATH_COUNT/COVID_CASE_COUNT as death_case_ratio
+    from `elated-guild-327520.covid.test_death_1d`
 )
 
 select 
         a.modzcta_name,
         a.modzcta,
         a.percentpositivity_7day,
+        a.people_positive,
         a.median_daily_test_rate,
         b.hospitalization_count_28day,
         b.hospitalization_rate_28day,
         b.death_count_28day,
         b.death_rate_28day,
-        b.daterange
+        b.daterange,
+        c.COVID_DEATH_COUNT,
+        c.death_case_ratio
 from test_7d as a
 full join hosp_death_28d as b
 on a.modzcta=b.modzcta
+full join case_death_1d as c
+on a.modzcta=c.modzcta
