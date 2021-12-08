4bf654fde838ec75c3af1a36831be61eff700050
johnatawnclementawn <johnathan.clementi1@gmail.com>
2021-12-08 01:21:32 -0500

    Add different pages

--- airflow/dags/report_generator/generate_report1.py
+++ airflow/dags/report_generator/generate_report1.py
@@ -60,7 +60,7 @@ def main(ds):
     print("Index.html report generated!")
 
 
-    # Render the auxiliary data into the templates.
+    # Render the deaths into the templates.
     deathsEnv = Environment(loader=PackageLoader('generate_report1'))
     deathsTemplate = deathsEnv.get_template('deaths.html')
     deathsOutput = deathsTemplate.render(
@@ -84,6 +84,58 @@ def main(ds):
             content_type='text/html,'
         )
     print("deaths.html report generated!")
+
+    # Render the cases into the templates.
+    casesEnv = Environment(loader=PackageLoader('generate_report1'))
+    casesTemplate = casesEnv.get_template('cases.html')
+    casesOutput = casesTemplate.render(
+        wk_all=wk_all_df.to_dict('list'),
+        daily_summary=daily_summary_df.to_dict('list'),
+        covid_map=covid_map_gdf.to_json(),
+        new_report=new_report_df.to_dict('list'),
+        top_test=top_test_df.to_dict('records'),
+        vac_accumulated_by_day=vac_accumulated_by_day_df.to_dict('list'),
+        breakthrough=breakthrough_df.to_dict('list'),
+        #death_treated=dh_28day.to_dict('list')
+    )
+
+    # Write the auxiliary report to the local folder, and upload to GCS.
+    with open(output_folder / 'cases.html', 'w') as local_file:
+        local_file.write(casesOutput)
+        local_file_to_gcs(
+            local_file_name=local_file.name,
+            gcs_bucket_name='anranz_cloudservices',
+            gcs_blob_name=f'{ds}/cases.html',
+            content_type='text/html,'
+        )
+    print("cases.html report generated!")
+
+    # Render the hospital into the templates.
+    hospitalEnv = Environment(loader=PackageLoader('generate_report1'))
+    hospitalTemplate = hospitalEnv.get_template('hospital.html')
+    hospitalOutput = hospitalTemplate.render(
+        wk_all=wk_all_df.to_dict('list'),
+        daily_summary=daily_summary_df.to_dict('list'),
+        covid_map=covid_map_gdf.to_json(),
+        new_report=new_report_df.to_dict('list'),
+        top_test=top_test_df.to_dict('records'),
+        vac_accumulated_by_day=vac_accumulated_by_day_df.to_dict('list'),
+        breakthrough=breakthrough_df.to_dict('list'),
+        #death_treated=dh_28day.to_dict('list')
+    )
+
+    # Write the auxiliary report to the local folder, and upload to GCS.
+    with open(output_folder / 'hospital.html', 'w') as local_file:
+        local_file.write(hospitalOutput)
+        local_file_to_gcs(
+            local_file_name=local_file.name,
+            gcs_bucket_name='anranz_cloudservices',
+            gcs_blob_name=f'{ds}/hospital.html',
+            content_type='text/html,'
+        )
+    print("hospital.html report generated!")
+
+
     print("Finished!")
 
 if __name__ == '__main__':
